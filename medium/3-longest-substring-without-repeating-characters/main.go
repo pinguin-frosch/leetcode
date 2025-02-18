86de00d8b22c1007ad173e2769ee183343d8b441
Gabriel Barrientos <gabri12.rubik@gmail.com>
2025-02-18 20:49:52 -0300

    medium-3 updated

--- medium/3-longest-substring-without-repeating-characters/main.go
+++ medium/3-longest-substring-without-repeating-characters/main.go
@@ -17,7 +17,6 @@ func lengthOfLongestSubstring1(s string) int {
 		counts[char]++
 		if counts[char] > 1 {
 			longest = max(longest, len(counts))
-			// TODO: only remove the repeated element
 			clear(counts)
 			l++
 			r = l
@@ -26,4 +25,19 @@ func lengthOfLongestSubstring1(s string) int {
 		r++
 	}
 	return max(longest, len(counts))
+}
+
+func lengthOfLongestSubstring2(s string) int {
+	l, r := 0, 0
+	seen := make(map[byte]int)
+	longest := 0
+	for ; r < len(s); r++ {
+		char := s[r]
+		if index, exists := seen[char]; exists && index >= l {
+			l = index + 1
+		}
+		seen[char] = r
+		longest = max(longest, r-l+1)
+	}
+	return longest
 }
